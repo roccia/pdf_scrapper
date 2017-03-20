2cb64fefb59015245d60fd9e3262cc767e9a1f01
yanweizhen <yanweizhen@yimeijian.cn>
2017-03-20 12:18:52 +0800

    finish

--- cn_info.rb
+++ cn_info.rb
@@ -4,13 +4,13 @@ require 'rubygems'
 require 'pdf/reader'
 require 'active_record'
 require 'mysql2'
+require 'open-uri'
 
-
-class CnInfo <  ActiveRecord::Base
+class CnInfo < ActiveRecord::Base
+  validates_uniqueness_of :url
 
   URL = "http://www.cninfo.com.cn/cninfo-new/announcement/query"
-
-  URL_PERFIX = "cninfo.com.cn"
+  URL_PERFIX = "http://www.cninfo.com.cn"
   INDUSTRY = {:农业 => "农、林、牧、渔业",
               :采矿业 => "采矿业",
               :制造业 => "制造业",
@@ -112,78 +112,107 @@ class CnInfo <  ActiveRecord::Base
           tabName: 'fulltext',
           seDate: "#{arg[3]} ~ #{arg[4]}"
       }
-      response = RestClient.post(URL, params)
+
+      begin
+        response = RestClient.post(URL, params)
+      rescue RestClient::ExceptionWithResponse => err
+        p "#############{err.response}############"
+      end
+
       rs = JSON.parse response
+      if rs.present?
+        p '############查询开始############'
+      else
+        p '############网络问题，请重试############'
+      end
+
       rs["announcements"].each do |s|
         announcementTitle = s["announcementTitle"]
         commpany_code = s["secCode"]
         commpany_name = s["secName"]
         adjunctUrl = s["adjunctUrl"]
+        p "$$$$$$$$$$$$ 开始读取PDF $$$$$$$$$$$$$$"
+        content = read_pdf("#{URL_PERFIX}/#{adjunctUrl}")
+        p "$$$$$$$$$$$$PDF内容为：#{content}$$$$$$$$$$$$$$"
         ary << {:industry => industry,
                 :plate => plate,
                 :category => category,
                 :title => announcementTitle,
                 :code => commpany_code,
                 :name => commpany_name,
-                :url =>"#{URL_PERFIX}/#{adjunctUrl}"
+                :url => "#{URL_PERFIX}/#{adjunctUrl}",
+                :content => content
         }
       end
     end
     ary
   end
 
-  def save_to_db
-    ActiveRecord::Base.establish_connection(
-        adapter:  'mysql2',
-        database: 'PDF',
-        username: 'meiuser',
-        password: '_m3iu$er_',
-        host:     'localhost'
-    )
-
+  def self.read_pdf(url)
+    content_ary = []
+    io = open(url)
+    reader = PDF::Reader.new(io)
+    reader.pages.each do |page|
+      content = page.text
+      content_ary << content
+    end
+    content_ary
   end
 
-  def self.get_data(*arg)
-    res  =   get_result(industry, plate,  report, start_time, end_time )
+  def self.save_to_db(res)
+    begin
+      ActiveRecord::Base.establish_connection(
+          adapter: 'mysql2',
+          database: 'cn_infos',
+          username: 'root',
+          password: '000000',
+          host: 'localhost'
+      )
+      ActiveRecord::Base.connection.active?
+      p "############数据库连接成功，开始导入数据############"
+    rescue Exception => e
+      p "Exception db connection :  #{e.message} "
+      raise "数据库连接失败"
+    end
+
     res.each do |s|
-      PdfTest.create(:title => s[:title], :company_code => s[:code], :company_name => s[:name], :url => s[:url])
+      CnInfo.create(:industry => s[:industry], :plate => s[:plate], :category => s[:category], :title => s[:title], :company_code => s[:code], :company_name => s[:name], :url => s[:url], :content => s[:content])
     end
   end
 
-
   def self.read_line
-  puts "选择行业----------"
-  puts "行业列表：#{INDUSTRY.keys}"
-  puts '-----------'
-  a =  gets.chomp
-   i_ary = a.split(',')
-   s = i_ary.map{|i| INDUSTRY[i.to_sym] }
-  industry =  s.map(&:inspect).join(';')
+    puts "选择行业----------"
+    puts "行业列表：#{INDUSTRY.keys}"
+    puts '-----------'
+    a = gets.chomp
+    i_ary = a.split(',')
+    s = i_ary.map { |i| INDUSTRY[i.to_sym] }
+    industry = s.map(&:inspect).join(';')
 
-  puts "选择板块----------"
-  puts "板块列表：#{PLATE.keys}"
-  puts '-----------'
-  b = gets.chomp
-  p_ary = b.split(',')
-  ps = p_ary.map{|i| PLATE[i.to_sym] }
-  plate = ps.map(&:inspect).join(';')
+    puts "选择板块----------"
+    puts "板块列表：#{PLATE.keys}"
+    puts '-----------'
+    b = gets.chomp
+    p_ary = b.split(',')
+    ps = p_ary.map { |i| PLATE[i.to_sym] }
+    plate = ps.map(&:inspect).join(';')
 
-  puts "选择报告类型----------"
-  puts "报告列表：#{CATEGORY.keys}"
-  puts '-----------'
-  c = gets.chomp
-  report = CATEGORY[c.to_sym]
+    puts "选择报告类型----------"
+    puts "报告列表：#{CATEGORY.keys}"
+    puts '-----------'
+    c = gets.chomp
+    report = CATEGORY[c.to_sym]
 
-  puts "选择起始时间----------"
-  start_time = gets.chomp
-  puts "选择结束时间----------"
-  end_time = gets.chomp
+    puts "选择起始时间----------"
+    start_time = gets.chomp
+    puts "选择结束时间----------"
+    end_time = gets.chomp
 
-  puts industry, plate,  report, start_time, end_time
-  get_result(industry, plate,  report, start_time, end_time )
+    res = get_result(industry, plate, report, start_time, end_time)
+    save_to_db(res)
   end
 
-  #  get_result("#{INDUSTRY[:农业]}", PLATE[:深市主板], CATEGORY[:年度报告], "2017-01-01", "2017-03-12")
+  read_line
 
 end
 
